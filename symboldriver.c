15a9462a3dd8540299744b15bbee235c6a15e6d0
Kirill Velovatiy <velovatiykirill@gmail.com>
2022-12-24 10:00:43 +0300

    Add test symboldriver

--- symboldriver.c
+++ symboldriver.c
@@ -1,4 +1,3 @@
-
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/fs.h>
@@ -27,7 +26,7 @@ int init_module(void)
 {
         Major = register_chrdev(0, DEVICE_NAME, &fops);
 	if (Major < 0) {
-	  printk(KERN_ALERT "Registering char device failed with %d\n", Major);
+	  printk(KERN_ALERT "Error %d\n", Major);
 	  return Major;
 	}
 	printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DEVICE_NAME, Major);
